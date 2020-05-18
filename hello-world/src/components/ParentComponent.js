e1b6a929a38ea6214ff2909114611130341eb7f2
sohelsorowar <sohelsorowar@gmail.com>
2020-05-18 21:44:53 +0600

    Methods as props

--- hello-world/src/components/ParentComponent.js
+++ hello-world/src/components/ParentComponent.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
-import ChildComponent from './components/ChildComponent'
+import ChildComponent from './ChildComponent'
 
 class ParentComponent extends Component {
 
-    constructor(props) {
-      super(props)
+  constructor(props) {
+    super(props)
 
     this.state={
-        parentName : 'Parent'
-      }
-      this.greetParent = this.greetParent.bind(this)
+      parentName:'Parent'
     }
+    this.greetParent = this.greetParent.bind(this)
+  }
 
-    greetParent(childName){
-      alert(`Hello ${this.state.parentName} from ${childName}`)
-    }
+greetParent(childName){
 
+  alert(`Hello ${this.state.parentName} from ${childName}`)
+}
 
   render() {
     return (
       <div>
       <ChildComponent greetHandler={this.greetParent}/>
+
       </div>
     )
   }
