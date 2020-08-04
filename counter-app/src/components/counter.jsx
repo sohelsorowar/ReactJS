95989fa2d252ed75bd324f2fa5afb70e8d83e343
sohelsorowar <sohelsorowar@gmail.com>
2020-08-04 15:13:42 +0600

    adding list

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="" />
+        <br></br> <br />
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+}
+
+export default Counter;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from './components/counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter/>
   </React.StrictMode>,
   document.getElementById("root")
 );
