076edc0dabf1b7345bf7f936f01c178354481e16
sohelsorowar <sohelsorowar@gmail.com>
2020-08-04 21:02:59 +0600

    return Message if empty

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,32 +7,20 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags!</p>;
+
     return (
-      <div>
-        <img src={this.state.imageUrl} alt="" />
-        <br></br> <br />
-        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+  render() {
+    return <div>{this.renderTags()}</div>;
   }
 }
 
