f97720cbff3ce073ab4d6efea398c1160559820c
sohelsorowar <sohelsorowar@gmail.com>
2020-08-05 09:41:45 +0600

    composing Counters

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,8 +11,9 @@ class Counter extends Component {
   // }
   
   //insted of using this constructor we can use => or arrow function 
-  handleIncrement =()=> {
-    this.state.count++;
+  handleIncrement = product => {
+    //this.state.count++;
+    console.log(product);
     this.setState({ count: this.state.count +1 });
   };
   render() {
@@ -20,7 +21,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,16 +4,12 @@ import "./index.css";
 
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Counters />,document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
