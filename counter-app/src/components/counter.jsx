5bef816a26206ec4cfc030c74c5e8020f2c67cd8
sohelsorowar <sohelsorowar@gmail.com>
2020-08-06 15:44:39 +0600

    passing children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,23 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.value,
   };
 
   // constructor(){
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
-  
-  //insted of using this constructor we can use => or arrow function 
-  handleIncrement = product => {
+
+  //insted of using this constructor we can use => or arrow function
+  handleIncrement = (product) => {
     //this.state.count++;
     console.log(product);
-    this.setState({ value: this.state.value +1 });
+    this.setState({ value: this.state.value + 1 });
   };
   render() {
+    console.log(this.props);
+
     return (
       <div>
+        <h4>Counter N0: {this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement()}
