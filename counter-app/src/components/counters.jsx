309a4c0a712bd79c16a22022cd13de15eeead6e6
sohelsorowar <sohelsorowar@gmail.com>
2020-08-07 09:33:00 +0600

    single source of state

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -8,7 +8,15 @@ class Counters extends Component {
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
-    ],
+    ]
+  };
+
+  handleReset = () =>{
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
   };
 
   handleDelete = counterId => {
@@ -19,6 +27,8 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        
+        <button  onClick={this.handleReset} className="btn btn-primary btn-sm m2">Reset</button>
       {this.state.counters.map((counter) => (
         <Counter 
         key={counter.id} 
