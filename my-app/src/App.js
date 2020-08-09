e30fd4365d9b9b4dcc8dba978bb37d7089cc1839
sohelsorowar <sohelsorowar@gmail.com>
2020-08-09 22:38:33 +0600

    my app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './mycomponents/Hello'
+
+function App() {
+  return(
+    <div className="App" >
+
+      <Hello></Hello>
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/mycomponents/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Hello(){
+    return(
+      
+            <h1>Hello I am from functional Components</h1>
+      
+    );
+}
+export default Hello
