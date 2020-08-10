8305c4668796e3656ca04990a41bb155181705f4
sohelsorowar <sohelsorowar@gmail.com>
2020-08-10 10:31:57 +0600

    class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './mycomponents/Hello'
+import Welcome from './mycomponents/Welcome'
+
 
 function App() {
   return(
     <div className="App" >
 
       <Hello></Hello>
+      <Welcome></Welcome>
     </div>
   )
 }
