6abedd6970263f04b291a26a6766061995eb93e7
sohelsorowar <sohelsorowar@gmail.com>
2020-08-10 21:35:09 +0600

    passing arguments to event handler

--- my-app/src/mycomponents/Hello.js
+++ my-app/src/mycomponents/Hello.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 function Hello(props){
 
-    function doThis(){
-     alert("button clicked");
+    function doThis(a){
+     alert(a);
      }
-
 
     return(
         
         <div>
             
-            <button onClick={doThis}>Click Me</button>
+            <button onClick={doThis.bind(this,"Hello i am from parameter")}>Click Me</button>
             <h1>Name: {props.name} and age : { props.age}</h1>
             
         </div> 
