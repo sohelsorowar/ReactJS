a735a82bf5dd5a5cf022ab066343e53b746c29dd
sohelsorowar <sohelsorowar@gmail.com>
2020-08-22 17:13:43 +0600

    text area

--- my-app/src/mycomponents/Forms.js
+++ my-app/src/mycomponents/Forms.js
@@ -6,17 +6,21 @@ class Forms extends Component {
 
         super()
         this.state={
-            username: "  "
+            username: "  ",
+            userInput:" "
         }
 
     }
 
     onChangeHandler=(event)=>{
-
         var myname = event.target.name;
         var myvalue = event.target.value;
         this.setState({[myname]:myvalue})
-
+    }
+    
+    onChangeHandler2=(event)=>{
+        var mytext = event.target.value;       
+        this.setState({userInput:mytext})
     }
 
     onSubmitHandler=()=>{
@@ -33,6 +37,9 @@ class Forms extends Component {
                     <input name="username" onChange={this.onChangeHandler} type="text" placeholder="User Name"></input>
                     <input type="submit" value="Submit now"></input>
                 </form>
+                <h1>Text Area</h1>
+                <textarea onChange={this.onChangeHandler2} placeholder="Please insert some text" ></textarea>
+                <p>{this.state.userInput}</p>
                 
             </div>
         );
