18972891d98857822c8a8d81734bd66ef0ca05d3
sohelsorowar <sohelsorowar@gmail.com>
2020-08-23 16:32:05 +0600

    select drop down

--- my-app/src/mycomponents/Forms.js
+++ my-app/src/mycomponents/Forms.js
@@ -7,7 +7,14 @@ class Forms extends Component {
         super()
         this.state={
             username: "  ",
-            userInput:" "
+            userInput:" ",
+            city1:"Dhaka",
+            city2:"Bogura",
+            city3:"Rajshahi",
+            city4:"Chitagong",
+            show:" ",
+            auto:"Bogura"
+
         }
 
     }
@@ -27,6 +34,11 @@ class Forms extends Component {
         alert(this.state.username);
     }
 
+    onChangeHandler3=(event)=>{
+       var SelectedValue= event.target.value;
+        this.setState({show:SelectedValue, auto:SelectedValue});
+    }
+
     render() {
         return (
             <div>
@@ -40,7 +52,16 @@ class Forms extends Component {
                 <h1>Text Area</h1>
                 <textarea onChange={this.onChangeHandler2} placeholder="Please insert some text" ></textarea>
                 <p>{this.state.userInput}</p>
-                
+                <br></br><br></br>
+
+                <h1>Select Drop Down</h1>
+                <p>{this.state.show}</p>
+                <select onChange={this.onChangeHandler3} value={this.state.auto}>
+                    <option>{this.state.city1}</option>
+                    <option>{this.state.city2}</option>
+                    <option>{this.state.city3}</option>
+                    <option>{this.state.city4}</option>
+                </select><br></br><br></br>
             </div>
         );
     }
