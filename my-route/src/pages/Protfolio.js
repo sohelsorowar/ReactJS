a6e880e26eed5e6ad3c1888a1f97fc8740ee4b3d
sohelsorowar <sohelsorowar@gmail.com>
2020-08-26 21:13:44 +0600

    css style.

--- my-route/src/pages/Protfolio.js
+++ my-route/src/pages/Protfolio.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import styles from '../Mycss.module.css';
+
 class Protfolio extends Component {
     render() {
         return (
             <div>
-                <h1>Protfolio</h1>
+                <h1 className={styles.TextStyle}>This is my Protfolio with CSS Module</h1>
             </div>
         );
     }
