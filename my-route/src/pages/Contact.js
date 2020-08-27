0619e70545c886f979ebe1945ada763bf5554150
sohelsorowar <sohelsorowar@gmail.com>
2020-08-27 11:57:07 +0600

    npm install node-sass

--- my-route/src/pages/Contact.js
+++ my-route/src/pages/Contact.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../My.scss';
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Contact</h1>
+                <h1 className="TextStyle2">This is Contact With SASS</h1>
             </div>
         );
     }
