3f1c1e4b25f7c48a311088a7708fc9bcda70eac6
sohelsorowar <sohelsorowar@gmail.com>
2020-08-28 23:03:50 +0600

    post to server

--- my-route/src/pages/Protfolio.js
+++ my-route/src/pages/Protfolio.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 
 import styles from '../Mycss.module.css';
+import axios from 'axios';
 
 class Protfolio extends Component {
+
+
+    constructor(){
+        super();
+        this.state={
+            postData:" ",
+            postResult:" "
+        }
+    }
+
+    onChangeHandler=(event)=>{
+        var myData=event.target.value;
+        this.setState({postData:myData});
+
+    }
+
+    onClickHandler=()=>{
+
+        axios.post('http://ss.rabbil.com/test.php',this.state.postData)
+        .then(response=>{
+            this.setState({postResult:response.data})
+            alert(this.state.postResult)
+        })
+        .catch(error=>{
+            alert("somethin wronf");
+        })
+
+    }
+
+
     render() {
         return (
             <div>
                 <h1 className={styles.TextStyle}>This is my Protfolio with CSS Module</h1>
+                <h2>post data</h2>
+                <input onChange={this.onChangeHandler} type="text"/>
+                <button onClick={this.onClickHandler}>Send</button>
+
             </div>
         );
     }
