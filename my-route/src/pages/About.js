a6033d04da0001ee0af0e2a447e33de43d97e4cd
sohelsorowar <sohelsorowar@gmail.com>
2020-08-28 22:08:44 +0600

    http request get data and post data

--- my-route/src/pages/About.js
+++ my-route/src/pages/About.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class About extends Component {
+
+
+    constructor(){
+        super()
+        this.state={
+            myData:[]
+        }
+    }
+
+
+    componentDidMount(){
+
+        axios.get('https://restcountries.eu/rest/v2/all')
+        .then(response=>{
+            this.setState({myData:response.data})
+
+        })
+        .catch(error=>{
+            console.log(error)
+
+        })
+
+
+    }
+
     render() {
+
+        const myList=this.state.myData
+        const countryName=myList.map((myList)=>{
+            return <li>{myList.name}</li>
+        })
         return (
             <div>
                 <h1>About</h1>
+                <h2>Country name</h2>
+                <ul>
+                    {countryName}
+                </ul>
             </div>
         );
     }
