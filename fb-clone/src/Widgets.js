006936a25c3effe674fb1ec6ee63e10bfab5e1db
sohelsorowar <sohelsorowar@gmail.com>
2020-09-15 01:06:46 +0600

    iframe

---
+++ fb-clone/src/Widgets.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Widgets.css';
+import Iframe from 'react-iframe'
+
+
+function Widgets() {
+    return (
+        <div className="widgets">
+            <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+             width="340"
+            height="100%"
+            style={{border: "none", overflow:"hidden"}}
+            scrolling="no"
+            allowTransparency="true"
+            allow="encrypted-media"
+
+            >
+                
+
+            </Iframe>
+            
+        </div>
+    )
+}
+
+export default Widgets
